ea8902fb2f571acfaf09ff2b1abc9d5674944581
_Enoabasi_Essien_ <enoabasidream@gmail.com>
2023-01-19 02:52:33 +0400

    adding the addTodo feature.

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-"use strict";
+const todos = ["Get groceries", "Wash car"];
 
-let todo1 = "Add name to donation list";
-let todo2 = "Register for football donation match";
-let todo3 = "Play as a striker in the donation list";
-let todo4 = "Score a hat-trick for my team in the donation game";
-let todo5 = "Adding a new todo to the list";
+render();
 
-let todos = [todo1, todo2, todo3, todo4, todo5];
+function addTodo() {
+	const textbox = document.getElementById("textbox-element");
+	const title = textbox.value;
+	todos.push(title);
 
-/* A function that display the todos to the DOM. */
-function addTodos(todoList) {
-	for (let i = 0; i < todoList.length; i++) {
-		let $element = document.createElement("div"); // create the element to add to the DOM
-		// Operation calls to be done to element which includes styles, and other forms of manipulation
-		$element.classList.add("todo-style"); // a css class-style added to the element
-		$element.innerHTML = todoList[i]; // takes all the operations performed between the create element and the innerHTML
-		document.body.appendChild($element); // setting the element in the document body
-	}
+	render();
 }
 
-/* function call */
-addTodos(todos);
+// A Todo render function - prints a todos to the screen and resets the state
+// of the old todo entry.
+function render() {
+	// Reset the state of the old list of todos
+	const resetList = document.getElementById("todo-render");
+	resetList.innerHTML = " ";
+
+	todos.forEach(function (todo) {
+		const element = document.createElement("div");
+		element.innerText = todo.toString();
+		const todoRender = document.getElementById("todo-render");
+		todoRender.appendChild(element);
+	});
+}
